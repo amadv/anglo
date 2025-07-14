9cdc44041ce2dc379172b16f6090138ebf4b5aee
Aaron Madved <aaronmadved@gmail.com>
2025-07-14 11:40:19 -0400

    example project

---
+++ hello-world.js
@@ -0,0 +1,8 @@
+// hello-world.js
+import { angloComponent } from './anglo.js';
+
+export const HelloWorld = angloComponent(() => ({
+  name: 'World',
+  show: true
+}));
+
---
+++ main.js
@@ -0,0 +1,6 @@
+// main.js
+import { bootstrapAnglo } from './anglo.js';
+
+// Start the framework with optional global state
+bootstrapAnglo();
+
